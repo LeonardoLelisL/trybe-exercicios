e7d473a49b5d84475fde4ced74f40f5193549dba
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-03-30 16:15:54 -0300

    triângulo com o lado invertido

---
+++ 1-fundamentos/secao-3-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio7.js
@@ -0,0 +1,18 @@
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let space = ' ';
+
+for (let index = 1; index <= n; index +=1){
+
+  for (let index2 = 0; index2 < n - index; index2 += 1) {
+    inputLine = inputLine + space;
+  }
+
+  for (let index3 = 0; index3 < index; index3 += 1){
+    inputLine = inputLine + symbol;
+  }
+
+  console.log(inputLine);
+  inputLine = '';
+}
