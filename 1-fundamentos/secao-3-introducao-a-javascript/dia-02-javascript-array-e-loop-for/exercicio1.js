e16e3a3f51ede308a45f6e762ea8146f02d4f8cc
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-03-29 22:43:04 -0300

    descobrindo quantos valores ímpares tem o array

--- 1-fundamentos/secao-3-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio1.js
+++ 1-fundamentos/secao-3-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio1.js
@@ -1,6 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-let highernumber = 0;
+let highernumber = numbers [0];
+let impar = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
@@ -10,6 +11,11 @@ for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] > highernumber) {
     highernumber = numbers[index];
   }
+
+  if (numbers[index] % 2 !== 0) {
+    impar += 1;
+  }
+
 }
 
 console.log('A soma é: ' + sum);
@@ -19,9 +25,15 @@ let average = sum/numbers.length;
 console.log('A média aritmética é: ' + average);
 
 if (average > 20) {
-  console.log('Valor maior que 20')  
+  console.log('Valor maior que 20') ;
 } else {
-  console.log('Valor menor ou igual a 20')
+  console.log('Valor menor ou igual a 20');
 }
 
 console.log('O maior valor é: ' + highernumber);
+
+if (impar > 0) {
+  console.log('O array possui ' + impar + ' valores ímpares');
+} else {
+  console.log('Nenhum valor ímpar encontrado');
+}
