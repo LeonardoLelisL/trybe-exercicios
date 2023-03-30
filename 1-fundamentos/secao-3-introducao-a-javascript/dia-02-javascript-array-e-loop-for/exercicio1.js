1861ea4dea0a808ea8cfa28aebd6c252de2d6599
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-03-29 22:51:01 -0300

    encontrando o menor valor do array

--- 1-fundamentos/secao-3-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio1.js
+++ 1-fundamentos/secao-3-introducao-a-javascript/dia-02-javascript-array-e-loop-for/exercicio1.js
@@ -1,19 +1,24 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
-let highernumber = numbers [0];
+let higherNumber = numbers[0];
 let impar = 0;
+let smallestNumber = numbers[0];
 
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
 
   sum = sum + numbers[index];
 
-  if (numbers[index] > highernumber) {
-    highernumber = numbers[index];
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
   }
 
   if (numbers[index] % 2 !== 0) {
     impar += 1;
+  }
+
+  if (numbers[index] < smallestNumber) {
+    smallestNumber = numbers[index];
   }
 
 }
@@ -30,10 +35,12 @@ if (average > 20) {
   console.log('Valor menor ou igual a 20');
 }
 
-console.log('O maior valor é: ' + highernumber);
+console.log('O maior valor é: ' + higherNumber);
 
 if (impar > 0) {
   console.log('O array possui ' + impar + ' valores ímpares');
 } else {
   console.log('Nenhum valor ímpar encontrado');
 }
+
+console.log('O menor valor é: ' + smallestNumber);
