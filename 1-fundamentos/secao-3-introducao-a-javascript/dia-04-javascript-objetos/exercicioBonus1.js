76367b37b85ca3456e5da1f6f4292a25ca0e79a2
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-04-08 00:53:12 -0300

    exercícios bônus - organizando lições 2

--- 1-fundamentos/secao-3-introducao-a-javascript/dia-04-javascript-objetos/exercicioBonus1.js
+++ 1-fundamentos/secao-3-introducao-a-javascript/dia-04-javascript-objetos/exercicioBonus1.js
@@ -19,8 +19,8 @@ let lesson3 = {
 };
 
 //Exercício 1
-console.log('Exercício 1')
-console.log();
+console.log('Exercício 1\n')
+
 function addKey(object, key, value) {
   object[key] = value;
 }
@@ -30,9 +30,7 @@ console.log(lesson2);
 
 //Exercício 2
 
-console.log();
-console.log('Exercício 2');
-console.log();
+console.log('\nExercício 2\n');
 
 function keyList(object) {
   return Object.keys(object);    
@@ -42,9 +40,7 @@ console.log(keyList(lesson2));
 
 //Exercício 3
 
-console.log();
-console.log('Exercício 3');
-console.log();
+console.log('\nExercício 3\n');
 
 function objSize(object) {
   return Object.keys(object).length;
@@ -54,9 +50,7 @@ console.log(objSize(lesson1));
 
 //Exercício 4
 
-console.log();
-console.log('Exercício 4');
-console.log();
+console.log('\nExercício 4\n');
 
 function valueList(object) {
   return Object.values(object);    
@@ -66,9 +60,7 @@ console.log(valueList(lesson3));
 
 //Exercício 5
 
-console.log();
-console.log('Exercício 5');
-console.log();
+console.log('\nExercício 5\n');
 
 let allLessons = Object.assign({}, lessonA = {lesson1}, lessonB = {lesson2}, lessonC = {lesson3});
 
@@ -76,9 +68,7 @@ console.log(allLessons);
 
 //Exercício 6
 
-console.log();
-console.log('Exercício 6');
-console.log();
+console.log('\nExercício 6\n');
 
 function numberOfStudents(object) {
   let students = 0;
@@ -92,9 +82,7 @@ console.log(numberOfStudents(allLessons));
 
 //Exercício 7
 
-console.log();
-console.log('Exercício 7');
-console.log();
+console.log('\nExercício 7\n');
 
 function getValueByNumber(object, number) {
   return Object.values(object)[number];
@@ -104,9 +92,7 @@ console.log(getValueByNumber(lesson1, 0));
 
 //Exercício 8
 
-console.log();
-console.log('Exercício 8');
-console.log();
+console.log('\nExercício 8\n');
 
 function verifyPair(object, key, value) {
   for (let key1 in object) {
