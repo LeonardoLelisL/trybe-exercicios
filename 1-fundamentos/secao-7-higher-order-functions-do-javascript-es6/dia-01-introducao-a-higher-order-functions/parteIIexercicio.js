7efda5eded0cc1dcd65d7f30ba400ba401e61aaa
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-05-08 16:31:43 -0300

    exercício 3 da parte II

--- 1-fundamentos/secao-7-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/parteIIexercicio.js
+++ 1-fundamentos/secao-7-higher-order-functions-do-javascript-es6/dia-01-introducao-a-higher-order-functions/parteIIexercicio.js
@@ -77,3 +77,19 @@ const smallerName = () => {
   return nameBook;
 }
 console.log(smallerName());
+
+// const expectedResult = {
+//   author: {
+//     birthYear: 1948,
+//     name: 'George R. R. Martin',
+//   },
+//   genre: 'Fantasia',
+//   id: 1,
+//   name: 'As Crônicas de Gelo e Fogo',
+//   releaseYear: 1991,
+// };
+
+const getNamedBook = () => {
+  return books.find((element) => element.name.length === 26);
+}
+console.log(getNamedBook());
