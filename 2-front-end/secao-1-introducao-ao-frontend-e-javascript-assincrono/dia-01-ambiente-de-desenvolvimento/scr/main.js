205aa3aa793612738ba092fbbd70b445cee00a18
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-05-23 00:03:55 -0300

    validações

---
+++ 2-front-end/secao-1-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/scr/main.js
@@ -0,0 +1,20 @@
+import validator from 'validator';
+
+const inputValue = document.querySelector('#value');
+const button = document.querySelector('#button');
+const option = document.querySelector('#option');
+const answer = document.querySelector('#answer');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const fields = {
+    cpf: validator.isTaxID(inputValue.value, 'pt-BR'),
+    hexColor: validator.isHexColor(inputValue.value),
+    email: validator.isEmail(inputValue.value),
+    uuid: validator.isUUID(inputValue.value, 4),
+    url: validator.isURL(inputValue.value),
+  };
+
+  answer.innerHTML = `A validação retornou ${fields[option.value]}`;
+});
