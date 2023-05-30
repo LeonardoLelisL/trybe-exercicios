ca3df12daef56c08f96c5e164d6a270e9173d3a6
Leonardo Lelis <leolelislopes@yahoo.com.br>
2023-05-30 01:05:34 -0300

    função calculateAge

--- 2-front-end/secao-2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/src/components/hello-world.tsx
+++ 2-front-end/secao-2-introducao-ao-react/dia-01-introducao-ao-react-e-ao-typescript/src/components/hello-world.tsx
@@ -1,9 +1,16 @@
 import ModuleDetails from './module-details';
 import Title from './title';
+import calculateAge from '../utils/age';
 
 function HelloWorld() {
+  const name = 'Leonardo Lelis';
+  const birth = '08/05/1987';
+  const age = calculateAge(birth);
+
   return (
     <div>
+      <h1>{name}</h1>
+      <p>{`Age ${age}`}</p>
       <Title />
       <ModuleDetails />
     </div>
